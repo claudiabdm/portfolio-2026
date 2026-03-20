11c28548b19c43196afe42366bc5bf206f7b62ed
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2026-03-20 15:21:06 +0100

    Fix site url

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://wwww.claudiabdm.com",
+    site: "https://www.claudiabdm.com",
     devToolbar: {
         'enabled': false
     },
